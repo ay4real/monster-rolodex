4e84fe24aa2027f7133ca84d2aa33ffc0e6683dd
ay4real <aogbede@ic-sol.net>
2020-01-18 22:33:53 +0100

    Updates

---
+++ precache-manifest.74a85b9e59c04dbb36d7b9d59c48a698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159843ec98fb6e4f1eae413c3dbc9971",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3b474a729cc5639a6cfe",
+    "url": "/monster-rolodex/static/css/main.b54bc41a.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monster-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b474a729cc5639a6cfe",
+    "url": "/monster-rolodex/static/js/main.ef32719d.chunk.js"
+  },
+  {
+    "revision": "97a0d41a4eb0ad4a7920",
+    "url": "/monster-rolodex/static/js/runtime-main.28e6e983.js"
+  }
+]);
